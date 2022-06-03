1c6dab5a45b1b60943173aca4895b8c5d9e2fd4c
yanmasa <ueda3ban@gmail.com>
2022-06-03 15:19:29 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount]=useState(0);
+
+  const handleCountUp = () =>{
+    setCount(count+1)
+  }
+  const handleCountDown = () =>{
+    setCount(count-1)
+  }
+  const handleCountReset = () =>{
+    setCount(0)
+  }
+  const handleCountReverse = () =>{
+    setCount((pre) =>{
+      return pre*(-1)
+    })
+  }
+  const handleCountDouble = () =>{
+    setCount(count*2)
+  }
+  const handleCountThreeDiv = () =>{
+    setCount( pre => (pre%3===0 ? pre/3 : pre))
+  }
   return (
     <div>
-      This is an template for ReactApp
+      <p>{count}</p>
+      <button onClick={handleCountUp}>up</button>
+      <button onClick={handleCountDown}>down</button>
+      <button onClick={handleCountReset}>Reset</button>
+      <button onClick={handleCountReverse}>Reverse</button>
+      <button onClick={handleCountDouble}>double</button>
+      <button onClick={handleCountThreeDiv}>divThree</button>
     </div>
   );
 }
